ce6ab82e14887950fddf57370bb1e2b5a5c3da56
Mikolaj <technik2213@gmail.com>
2019-06-28 21:20:34 +0200

    Loop added

--- ZaDuzoZaMalo/ZaDuzoZaMalo/Program.cs
+++ ZaDuzoZaMalo/ZaDuzoZaMalo/Program.cs
@@ -4,27 +4,43 @@ namespace ZaDuzoZaMalo
 {
     class Program
     {
+        private static bool PlayAgain = true;
         static void Main(string[] args)
         {
-            Game g = new Game(1, 10);
-            
-            Console.WriteLine("Podaj liczbe:");
-            Game.Odp odp = g.Ocena(int.Parse(Console.ReadLine()));
-            Console.WriteLine( odp );
-            while (odp != Game.Odp.Trafiono)
+            do
             {
-                odp = g.Ocena((int.Parse(Console.ReadLine())));
-                Console.WriteLine(odp);
-            }
-            /*if (odp != Game.Odp.Trafiono)
-                odp = g.Ocena(7);
-            if (odp != Game.Odp.Trafiono)
-                odp = g.Ocena(3);*/
+                Game g = new Game(1, 10);
 
-            foreach( var ruch in g.Historia )
-            {
-                Console.WriteLine( $"{ruch.propozycja} {ruch.odp} {ruch.kiedy}");
-            }
+                Console.WriteLine("Podaj liczbe:");
+                Game.Odp odp = g.Ocena(int.Parse(Console.ReadLine()));
+                Console.WriteLine(odp);
+                while (odp != Game.Odp.Trafiono)
+                {
+                    odp = g.Ocena((int.Parse(Console.ReadLine())));
+                    Console.WriteLine(odp);
+                }
+
+                Console.WriteLine("--------------------\n");
+                Console.WriteLine("Twoja ilość punktów wynosi: ");
+                Console.WriteLine(g.LicznikRuchow);
+                Console.WriteLine("--------------------\n");
+
+                foreach (var ruch in g.Historia)
+                {
+                    Console.WriteLine($"{ruch.propozycja} {ruch.odp} {ruch.kiedy}");
+                }
+                
+                Console.WriteLine("Czy chcesz zagrać ponownie? tak/nie");
+                var response = Console.ReadLine();
+
+                if (response == "tak")
+                    PlayAgain = true;
+                else
+                {
+                    PlayAgain = false;
+                }
+            } while (PlayAgain is true);
+
         }
     }
 }
