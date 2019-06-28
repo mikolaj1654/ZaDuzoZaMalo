1461729d43f8829150f3842f9bcca88fa860ae27
Mikolaj <technik2213@gmail.com>
2019-06-28 20:38:51 +0200

    Created project

---
+++ ZaDuzoZaMalo/ZaDuzoZaMalo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZaDuzoZaMalo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
