f007fccf735bbbd54caac82074209a386411c94b
Mikolaj <technik2213@gmail.com>
2019-06-29 07:47:13 +0200

    remove unused files

--- ZaDuzoZaMalo/ZaDuzoZaMalo/Models/PlayerModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ZaDuzoZaMalo.Models
-{
-    public class PlayerModel
-    {
-        public string Name { get; set; }
-        public string Score { get; set; }
-        public string Id { get; set; }
-    }
-}
